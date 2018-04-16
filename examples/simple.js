c1a3973982177b5c998efa95c4c6332312c3c508
Luciano José <luciano.jsjr@gmail.com>
2018-04-15 23:16:00 -0300

    Update Simple Example

--- examples/simple.js
+++ examples/simple.js
@@ -1,6 +1,35 @@
 const fs = require('fs');
 const GreatReport = require('../lib/great-report');
 
+const getColumns = () => {
+	return [{
+		name: 'Label'
+	}, {
+		name: 'Date'
+	}, {
+		name: 'Company'
+	}, {
+		name: 'User Name'
+	}]
+};
+
+const getValues = () => {
+	let values = [];
+	
+	for (let rowNumber = 0; rowNumber < 330; rowNumber++) {
+		values.push({
+			name: `Row ${rowNumber}`,
+			date: '2010-10-12',
+			company_name: 'The Company',
+			user: {
+				name: `User ${rowNumber}`
+			}
+		});
+	}
+	
+	return values;
+};
+
 const createReport = () => {
 	let greatReport;
 	
@@ -16,6 +45,7 @@ const createReport = () => {
 			report_name: 'New Report'
 		});
 		
+		greatReport.renderTable(getColumns(), getValues(), ['name', 'date', 'company_name', 'user.name']);
 		greatReport.create('./simple.pdf').then(data => console.log(data)).catch(err => console.log(err));
 	});
 };
