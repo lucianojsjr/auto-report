b8e35ff8c5c5a376985b05287a421151c0cba297
Luciano José <luciano.jsjr@gmail.com>
2018-04-29 22:09:54 -0300

    Create a sample with css and table

---
+++ examples/table/pdf-table.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const AutoReport = require('../../index');
+const AutoReportPDF = new AutoReport.PDF();
+
+const getColumns = () => {
+	return [{
+		name: 'Date'
+	}, {
+		name: 'Name'
+	}, {
+		name: 'Itens'
+	}, {
+		name: 'Total Price'
+	}];
+};
+
+const getValues = () => {
+	let values = [];
+	
+	for (let rowNumber = 1; rowNumber < 80; rowNumber++) {
+		values.push({
+			date: '10/10/2010',
+			name: `Sr(a). Number ${rowNumber}`,
+			itens: `${rowNumber + 1} Shoes,${rowNumber + 2} Glass, and ${rowNumber + 3} Pants`,
+			price: `R$ ${(rowNumber * 1.12).toFixed(2)}`
+		});
+	}
+	
+	return values;
+};
+
+const createReport = () => {
+	fs.readFile('./pdf-table-template.html', 'utf-8', (err, template) => {
+		AutoReportPDF.init(template);
+		AutoReportPDF.config({
+			charset: 'utf-8',
+			css: ['./pdf-table.css']
+		});
+		
+		AutoReportPDF.render('company_name', 'Custom Company');
+		AutoReportPDF.render('report_name', 'Sale History');
+		AutoReportPDF.render('date', 'Report Date: 20/10/2010');
+		AutoReportPDF.render('logo_url', 'https://cdn.iconscout.com/public/images/icon/free/png-512/financial-analysis-report-card-reporting-valuation-3e6ec64bf68be326-512x512.png');
+		
+		AutoReportPDF.renderTable(getColumns(), getValues(), {
+			tag: 'table',
+			class: 'default-table',
+			properties: ['date', 'name', 'itens', 'price']
+		});
+		
+		AutoReportPDF.create('./pdf-table.pdf').then(data => console.log(data))
+		  .catch(err => console.log(err));
+	});
+};
+
+createReport();
+
